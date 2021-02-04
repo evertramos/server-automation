bd637d1a7bbdd986ddedafa6af1ad2f9e4bd23e6
Evert Ramos <evert.ramos@cgu.gov.br>
2021-02-03 23:20:39 -0300

    Check if .env file exists in current folder

---
+++ z_example-base/localscript/check-local-env-file.sh
@@ -0,0 +1,32 @@
+#-----------------------------------------------------------------------
+#
+# Server Automation - https://github.com/evertramos/server-automation
+#
+# Developed by
+#   Evert Ramos <evert.ramos@gmail.com>
+#
+# Copyright Evert Ramos
+#
+#-----------------------------------------------------------------------
+#
+# Be carefull when editing this file, it is part of a bigger script!
+#
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# This script has one main objective:
+# 1. Check if the .env file exists in the current folder
+#-----------------------------------------------------------------------
+
+checklocalenvfile() 
+{
+    [[ "$DEBUG" == true ]] && echo "Check if local '.env' file is set."
+
+    if [[ -e .env ]]; then
+        source .env
+    else 
+        MESSAGE="'.env' file not found! \n Cheers!"
+        return 1
+    fi
+}
+
