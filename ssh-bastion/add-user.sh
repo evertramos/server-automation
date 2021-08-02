a35caa219b98e871de632a7082b52ced1f4007bb
Evert Ramos <evert.ramos@cgu.gov.br>
2021-08-02 08:20:53 -0300

    ssh-bastion - update name for more meaningful expression

--- ssh-bastion/add-user.sh
+++ ssh-bastion/add-user.sh
@@ -2,14 +2,14 @@
 
 #-----------------------------------------------------------------------
 #
-# Add user to the ssh-manager container (docker-ssh-manager)
+# Add user to the ssh-bastion container (docker-ssh-bastion)
 #
-# Repo: https://github.com/evertramos/docker-ssh-manager
+# Repo: https://github.com/evertramos/docker-ssh-bastion
 #
 # Developed by
 #   Evert Ramos <evert.ramos@gmail.com>
 #
-# Copyright Evert Ramos
+# Copyright Evert Ramos 
 #
 #-----------------------------------------------------------------------
 
@@ -43,19 +43,19 @@ while [[ $# -gt 0 ]]
 do
     case "$1" in
 
-        # ssh-manager container name
+        # ssh-bastion container name
         -c)
-        ARG_SSH_MANAGER="${2}"
-        if [[ $ARG_SSH_MANAGER == "" ]]; then
+        ARG_SSH_BASTION="${2}"
+        if [[ $ARG_SSH_BASTION == "" ]]; then
             echoerror "Invalid option for -c";
             break;
         fi
         shift 2
         ;;
-        --ssh-manager=*)
-        ARG_SSH_MANAGER="${1#*=}"
-        if [[ $ARG_SSH_MANAGER == "" ]]; then
-            echoerror "Invalid option for --ssh-manager";
+        --ssh-bastion=*)
+        ARG_SSH_BASTION="${1#*=}"
+        if [[ $ARG_SSH_BASTION == "" ]]; then
+            echoerror "Invalid option for --ssh-bastion";
             break;
         fi
         shift 1
@@ -97,9 +97,9 @@ do
         shift 1
         ;;
 
-        # User's public key
+        # User public key (ssh-key)
         -k)
-        ARG_KEY_STRING="${2}"
+        ARG_KEY_STRING="${2%/}"
         if [[ $ARG_KEY_STRING == "" ]]; then
             echoerror "Invalid option for -k";
             break;
@@ -108,6 +108,7 @@ do
         ;;
         --key-string=*)
         ARG_KEY_STRING="${1#*=}"
+        ARG_KEY_STRING="${ARG_KEY_STRING%/}"
         if [[ $ARG_KEY_STRING == "" ]]; then
             echoerror "Invalid option for --key-string";
             break;
@@ -115,27 +116,34 @@ do
         shift 1
         ;;
 
-        # Sites container is an array, you might add it multiple times
-        -s)
-        ARG_SITES_CONTAINERS+=("${2}")
-        if [[ $ARG_SITES_CONTAINERS == "" ]]; then
-            echoerror "Invalid option for -s";
+        # Instead user key you might inform a key file
+        -f)
+        ARG_KEY_FILE="${2%/}"
+        if [[ $ARG_KEY_FILE == "" ]]; then
+            echoerror "Invalid option for -f";
             break;
         fi
         shift 2
         ;;
-        --site-container=*)
-        ARG_SITES_CONTAINERS+=("${1#*=}")
-        if [[ $ARG_SITES_CONTAINERS == "" ]]; then
-            echoerror "Invalid option for --site-container";
+        --key-file=*)
+        ARG_KEY_FILE="${1#*=}"
+        ARG_KEY_FILE="${ARG_KEY_FILE%/}"
+        if [[ $ARG_KEY_FILE == "" ]]; then
+            echoerror "Invalid option for --key-file";
             break;
         fi
         shift 1
         ;;
 
-        # Array of sites coming from the add user script
-        --sites-from-adduser-script=*)
-        ARG_SITES_FROM_ADDUSER_SCRIPT=(${1#*=})
+        # Generate new key for the user - @todo (improvement)
+#        --key-generate)
+#        GENERATE_KEY=true
+#        shift 1
+#        ;;
+
+        # Do not run the user-grant-access script
+        --add-user-only)
+        ADD_USER_ONLY=true
         shift 1
         ;;
 
@@ -161,11 +169,11 @@ do
         shift 1
         ;;
         -h | --help)
-        usage_grantuseraccess
+        usage_adduser
         ;;
         *)
         echoerror "Unknown argument: $1"
-        usage_grantuseraccess
+        usage_adduser
         exit 0
         ;;
     esac
@@ -179,7 +187,7 @@ done
 #run_function check_local_env_file
 
 # Specific PID File if needs to run multiple scripts
-LOCAL_NEW_PID_FILE=${PID_FILE_NEW_SITE:-".ssh_grant_user_access.pid"}
+LOCAL_NEW_PID_FILE=${PID_FILE_NEW_SITE:-".ssh_add_user.pid"}
 if [[ $ARG_PID_TAG == "" ]]; then
   NEW_PID_FILE=${LOCAL_NEW_PID_FILE}
 else
@@ -220,42 +228,50 @@ local_undo_restore()
 #-----------------------------------------------------------------------
 
 #-----------------------------------------------------------------------
-# ssh-manager container name (-c|--ssh-manager=|ARG_SSH_MANAGER|SSH_MANAGER)
+# Sites container name (-s|--site-container=|ARG_SITES_CONTAINERS [ARRAY])
+#
+# This options is used to inform which containers the newly created user
+# will have access granted, so you might inform in this script and do
+# one time job. All validations are done in grant-user-access script
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# ssh-bastion container name (-c|--ssh-bastion=|ARG_SSH_BASTION|SSH_BASTION)
 #
 # This is the container which will hold all ssh connections, it should not
 # have the docker socket mounted into it, or you might face a greta risk
 # of being hacked, once user might gain access to your host server ⚠
 #-----------------------------------------------------------------------
-SSH_MANAGER="${ARG_SSH_MANAGER:-ssh-manager}"
+SSH_BASTION="${ARG_SSH_BASTION:-ssh-bastion}"
 
-# Check if ssh-manager exists in your environment
-run_function docker_check_container_exists $SSH_MANAGER
+# Check if ssh-bastion exists in your environment
+run_function docker_check_container_exists $SSH_BASTION
 
 if [[ "$DOCKER_CONTAINER_EXISTS" != true ]]; then
-    echoerror "You must have ssh-manager running, and '$SSH_MANAGER' does not exist in this server. \
+    echoerror "You must have ssh-bastion running, and '$SSH_BASTION' does not exist in this server. \
       \nPlease inform the correct container name for this service or check the link below: \
-      \nhttps://github.com/evertramos/docker-ssh-manager/ \
-      \nif you do have it runnig please inform the container name by the option '--ssh-manager=YOUR_CONTAINER_NAME'."
+      \nhttps://github.com/evertramos/docker-ssh-bastion/ \
+      \nif you do have it runnig please inform the container name by the option '--ssh-bastion=YOUR_CONTAINER_NAME'."
 fi
 
-# Check if SSH_MANAGER is running
-run_function docker_check_container_is_running $SSH_MANAGER
+# Check if SSH_BASTION is running
+run_function docker_check_container_is_running $SSH_BASTION
 
 if [[ "$DOCKER_CONTAINER_IS_RUNNING" != true ]]; then
-    echoerror "The container '$SSH_MANAGER' exist in your envronment but it seems it's not running. But if you do \
-      \nhave it runnig please inform the correct container name by the option '--ssh-manager=YOUR_CONTAINER_NAME'."
+    echoerror "The container '$SSH_BASTION' exist in your envronment but it seems it's not running. But if you do \
+      \nhave it runnig please inform the correct container name by the option '--ssh-bastion=YOUR_CONTAINER_NAME'."
 fi
 
 #-----------------------------------------------------------------------
 # Username (-u|--user-name=|ARG_USER_NAME|USER_NAME)
 #
 # The username will be used to connect a user from outside of your network
-# into the ssh-manager container, where will be able to connect to other
+# into the ssh-bastion container, where will be able to connect to other
 # containers in your docker network, only those you granted access to
 #-----------------------------------------------------------------------
 if [[ $ARG_USER_NAME == "" ]]; then
 
-    # Request user input - username
+    # Request user input - username 
     run_function common_read_user_input "Please enter the username:"
 
     # Check if user input is empty
@@ -269,121 +285,74 @@ else
     USER_NAME=$(echo $ARG_USER_NAME | tr -dc '[:alnum:]\n\r' | tr '[:upper:]' '[:lower:]')
 fi
 
-# Check if user already exists in the ssh-manager
-run_function docker_check_user_exists_in_container $SSH_MANAGER $USER_NAME
-if [[ "$DOCKER_USER_EXISTS_IN_CONTAINER" != true ]]; then
-    echoerror "The user '$USER_NAME' does not exist in container the ssh-container: '$SSH_MANAGER'.\
-      \nIf you want to grant access to this user to any other container in your environment, firt add the user as following: \
-      \n./add-user.sh --user-name=$USER_NAME"
+# Check if user already exists in the ssh-bastion
+run_function docker_check_user_exists_in_container $SSH_BASTION $USER_NAME
+if [[ "$DOCKER_USER_EXISTS_IN_CONTAINER" == true ]]; then
+    echoerror "The user '$USER_NAME' already exist in container the ssh-container: '$SSH_BASTION'.\
+      \nIf you want to grant access to this user to any other container in your environment, please run the following: \
+      \n./grant-user-access.sh --user-name=$USER_NAME"
 fi
 
 #-----------------------------------------------------------------------
 # User's key (-k|--key-string=|ARG_KEY_STRING|USER_SSH_KEY)
+#            (-f|--key-file=|ARG_KEY_FILE|USER_SSH_KEY)
 #
-# This key will be used to grant access to all containers informed in
-# '--site-container' option, if you don't set this option manually
-# the default user's key from ssh-manager container will be used
+# The user created will only have access the ssh-bastion using a ssh-key,
+# so, you must get the user's public key or generate one for him before
+# run this script. This is a secure matter, please don't ignore it.
 #-----------------------------------------------------------------------
-if [[ $ARG_KEY_STRING == "" ]]; then
-
-    # Get user's pub key
-    run_function docker_get_user_ssh_pub_key $SSH_MANAGER $USER_NAME
-
-    USER_SSH_KEY="$DOCKER_USER_SSH_PUB_KEY"
-
-    if [[ $USER_SSH_KEY == "" ]]; then
-        echoerror "We could not find the pub key for the user '$USER_NAME' in container '$SSH_MANAGER'"
-    fi
-else
-    USER_SSH_KEY="${ARG_KEY_STRING}"
-fi
-
-#-----------------------------------------------------------------------
-# Sites containers' name (-s|--site-container=|ARG_SITES_CONTAINERS [ARRAY])
-#
-# This options is used to inform which containers the newly created user
-# will have access granted, so you might inform in this script and do
-# one time job. All validations are done in grant-user-access script
-#-----------------------------------------------------------------------
-
-# Check if sites containers' name were sent from '--sites-from-adduser-script' option
-if [[ $ARG_SITES_FROM_ADDUSER_SCRIPT == "" ]]; then
+if [[ $ARG_KEY_STRING == "" ]] && [[ $ARG_KEY_FILE == "" ]]; then
     
-    # Check if Site Argument was passed
-    if [[ $ARG_SITES_CONTAINERS == "" ]]; then
-        
-        # Get list of containers
-        run_function docker_list_container
+    # Get pasted user ssh pub key
+    run_function common_read_user_input "Please paste user's ssh pub key (${red}SINGLE LINE${reset}) or hit ENTER to exit:"
 
-        # Show running site to be chosen by the user
-        run_function select_multiple_data "${DOCKER_LIST_CONTAINER_RESPONSE[*]}"
-        
-        SITES_CONTAINERS=(${USER_MULTIDATA_SELECTIONS[@]})
+    if [[ $USER_INPUT_RESPONSE == "" ]]; then 
+        echoerror "You must past the user's ssh pub key or inform one of the options: --key-string='your_ssh_key_string' or --key-file='path_to_ssh_key_file'."
     else
-        SITES_CONTAINERS=("${ARG_SITES_CONTAINERS[@]}")
+        ARG_KEY_STRING=$USER_INPUT_RESPONSE
     fi
-else
-    SITES_CONTAINERS=(${ARG_SITES_FROM_ADDUSER_SCRIPT[@]})
+    
+elif [[ $ARG_KEY_STRING != "" ]] && [[ $ARG_KEY_FILE != "" ]]; then
+    echoerror "You must inform only one of the options: --key-string='your_ssh_key_string' or --key-file='path_to_ssh_key_file'"
 fi
 
-# Check if Sites Container is empty 
-if [[ $SITES_CONTAINERS == "" ]]; then
-    echoerror "Site must be informed in order to grant user access. Please use --site-container option. \
-      \nYou might set it multiple times as --site-container=site1 --site-container=site2 --site-container=site3 \
-      \nor choose as many containers as you need from the list"
-fi
+# Get the USER_SSH_KEY
+[[ $ARG_KEY_STRING != "" ]] && USER_SSH_KEY=$ARG_KEY_STRING
+[[ $ARG_KEY_FILE != "" ]] && USER_SSH_KEY=$(cat ${ARG_KEY_FILE})
 
 #-----------------------------------------------------------------------
 # Confirm action
 #-----------------------------------------------------------------------
-if [[ ! "$SILENT" == true  ]] || [[ ! "$REPLY_YES" == true ]]; then
-    run_function confirm_user_action "You are allowing the user '$USER_NAME' to access the container(s) '${SITE_CONTAINER_STRING// / - }'. \
+if [[ "$SILENT" != true ]] && [[ "$REPLY_YES" != true ]]; then
+    run_function confirm_user_action "You are creating the user '$USER_NAME' in the container '$SSH_BASTION'. \
       \nAre you sure you want to continue?" true
 fi
 
 #-----------------------------------------------------------------------
-# Loop through the sites containers and add users' key to the main user
+# Create the user in the ssh-bastion container
 #-----------------------------------------------------------------------
-for SITE_CONTAINER in "${SITES_CONTAINERS[@]}"; do
+run_function docker_add_user_with_key $SSH_BASTION $USER_NAME "$USER_SSH_KEY"
 
-    # Check if site is running
-    run_function docker_check_container_is_running $SITE_CONTAINER
-
-    if [[ "$DOCKER_CONTAINER_IS_RUNNING" == true ]]; then
-
-        # Check if openssh-server is running
-        run_function docker_check_service_is_running_with_procps $SITE_CONTAINER "ssh"
-
-        if [[ "$DOCKER_SERVICE_IS_RUNNING" != true ]]; then
-            # Install openssh-server
-            run_function docker_install_package_with_apt $SITE_CONTAINER "openssh-server"
-
-            # Config openssh-server
-            run_function local_config_openssh_service $SITE_CONTAINER
-
-            # Start openssh-server
-            run_function docker_start_service_with_service $SITE_CONTAINER "ssh"
-        fi
-
-        # Save users' ssh pub key in site container
-        run_function docker_add_ssh_key_to_main_user "$SITE_CONTAINER" "$USER_SSH_KEY"
-    else
-        echowarning "Container '$SITE_CONTAINER' is not running."
-    fi
-done
-
-#-----------------------------------------------------------------------
-# Final message
-#-----------------------------------------------------------------------
-if [[ ! "$SILENT" == true  ]]; then
-    echosuccess "You can access the sites selected using the key from '$USER_NAME': \
-      \nssh $USER_NAME@$SSH_MANAGER -t 'ssh $SITE_CONTAINER'"
+# Verify if user was created as expected
+run_function docker_check_user_exists_in_container $SSH_BASTION $USER_NAME
+if [[ ! "$DOCKER_USER_EXISTS_IN_CONTAINER" == true ]]; then
+    echoerror "The user '$USER_NAME' could not be created in container '$SSH_BASTION' for an unexppected reason. \
+      \nPlease try again with option '--debug' to see if there is any specific errors."
 fi
 
+#-----------------------------------------------------------------------
+# Grant access to the newly created user, unless if you set --add-user-only
+#-----------------------------------------------------------------------
+if [[ "$ADD_USER_ONLY" != true ]]; then
+    if [[ "$SILENT" == true ]] && [[ "$REPLY_YES" == true ]]; then
+        $SCRIPT_PATH/grant-user-access.sh "--ssh-bastion=$SSH_BASTION" "--user-name=$USER_NAME" "--sites-from-adduser-script=${ARG_SITES_CONTAINERS[*]}" "--silent" "--yes"
+    elif [[ "$REPLY_YES" == true ]]; then
+        $SCRIPT_PATH/grant-user-access.sh "--ssh-bastion=$SSH_BASTION" "--user-name=$USER_NAME" "--sites-from-adduser-script=${ARG_SITES_CONTAINERS[*]}" "--yes"
+    elif [[ "$SILENT" == true ]]; then
+        $SCRIPT_PATH/grant-user-access.sh "--ssh-bastion=$SSH_BASTION" "--user-name=$USER_NAME" "--sites-from-adduser-script=${ARG_SITES_CONTAINERS[*]}" "--silent"
+    else
+        $SCRIPT_PATH/grant-user-access.sh "--ssh-bastion=$SSH_BASTION" "--user-name=$USER_NAME" "--sites-from-adduser-script=${ARG_SITES_CONTAINERS[*]}"
+    fi
+fi 
+
 exit 0
-
-
-# if user does not exist ask to create one
-
-
-# corrigir permissão da pasta .ssh para 700 e do arquivo .ssh/config para 644
